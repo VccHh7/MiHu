3a4d7b6e1189009b7e24bbf10d100965b993664d
VccHh7 <944654981@qq.com>
2022-03-21 16:29:16 +0800

    新建了test

---
+++ test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+  html,
+  body,
+  div,
+  span,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  ul,
+  ol,
+  li,
+  /*删除浏览器默认值*/
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
+  html,
+  body,
+  #app {
+    width: 100%;
+    height: 100%;
+    font-family: "微软雅黑";
+  }
+
+  ul,
+  ol {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+</style>
